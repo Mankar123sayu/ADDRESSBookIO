731360e6d4188f3c8c47676ee5abdcd8c8a78da9
Sayunkta Mankar <sayumankar77@gmail.com>
2023-06-26 14:04:25 +0530

    Add

---
+++ AddressBookIo/AddressBookMain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookIo
+{
+    public class AddressBookMain
+    {
+        
+            Contact contact = new Contact();
+            List<Contact> main = new List<Contact>();
+    }
+}
